b7b7d64c86e3042272c5dd4a4eba3b6c423fca9c
Emiliano Perez <emiperez.dev@gmail.com>
2024-02-01 08:34:34 -0600

    Changing password message

--- src/start/config.ts
+++ src/start/config.ts
@@ -6,5 +6,5 @@ export const PORT = process.env.PORT || 3000;
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const DB_NAME = process.env.DB_NAME || "companydb";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "Soyunhit1!";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "YOURPASSWORD";
 export const DB_HOST = process.env.DB_HOST || "localhost";
